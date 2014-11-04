c4d235a45acb8db93c94bf8f498ed734df9c2857
Jack Cheng <jack.cheng@commercan.com>
2014-11-03 16:36:55 -0800

    WebAPI demo

---
+++ WebServices.Web/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebServices.Web.Models;
+
+namespace WebServices.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+
+        public ActionResult Index()
+        {
+            return View(repo.GetAll());
+        }
+
+        public ActionResult Add(Reservation item)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Add(item);
+                return RedirectToAction("Index");
+            }
+            return View("Index");
+        }
+
+        public ActionResult Remove(int id)
+        {
+            repo.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(Reservation item)
+        {
+            if (ModelState.IsValid &&
+                repo.Update(item))
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ WebServices.Web/Controllers/WebController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServices.Web.Models;
+
+namespace WebServices.Web.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+
+        // GET api/<controller>
+        public IEnumerable<Reservation> GetAllReservataions()
+        {
+            return repo.GetAll();
+        }
+
+        // GET api/<controller>/5
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+
+        // POST api/<controller>
+        public Reservation Post(Reservation item)
+        {
+            return repo.Add(item);
+        }
+
+        // PUT api/<controller>/5
+        public bool PutReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
