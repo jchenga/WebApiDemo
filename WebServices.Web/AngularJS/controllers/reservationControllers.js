475fd51d60c5ae4c499855cec4e67cc384e8027a
Jack Cheng <jack.cheng@commercan.com>
2014-11-05 16:28:53 -0800

    added angularjs codes

---
+++ WebServices.Web/AngularJS/controllers/reservationControllers.js
@@ -0,0 +1,38 @@
+﻿angular.module("reservationApp")
+    .constant("reservationUrl", "http://localhost:49955/api/web/")
+    .controller("reservationCtrl", function($scope, $resource, reservationUrl) {
+        $scope.reservationsResource = $resource(reservationUrl + ":id", { id: "@ReservationId" });
+
+    $scope.listReservations = function() {
+        $scope.reservations = $scope.reservationsResource.query();
+    };
+    $scope.deleteReservation = function(reservation) {
+        reservation.$delete().then(function() {
+            $scope.reservations.splice($scope.reservations.indexOf(reservation));
+        });
+    };
+
+    $scope.createReservation = function (reservation) {
+        new $scope.reservationsResource(reservation).$save().then(function(newReservation) {
+            $scope.reservations.push(newReservation);
+            $scope.editedReservation = null;
+        });
+    };
+
+    $scope.updateReservation = function(reservation) {
+        reservation.$save();
+        $scope.editedReservation = null;
+    };
+
+    $scope.startEdit = function(reservation) {
+        $scope.editedReservation = reservation;
+    };
+
+    $scope.cancelEdit = function() {
+        $scope.editedReservation = null;
+    };
+
+    $scope.listReservations();
+
+
+});
